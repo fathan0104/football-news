d09ea71dd1a800fc4cf909aaac51bba04099a0b8
fathan0104 <fathan.alfahrezi@ui.ac.id>
2025-09-03 13:53:34 +0700

    Complete tutorial 1: Django MVT implementation

--- football_news/settings.py
+++ football_news/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+# SECURITY WARNING: keep th e secret key used in production secret!
 SECRET_KEY = 'django-insecure-z+ca$ukreb-u(tzpnmmbscu$-69d2qqj4@*x))5cw9%o1*#&uc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
