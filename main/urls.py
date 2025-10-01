5f4c82930a5d8b1a8a2e82367385f9e52b197b79
fathan0104 <fathan.alfahrezi@ui.ac.id>
2025-10-01 13:50:17 +0700

    tutorial 5 selesai

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from main.views import logout_user
 from django.contrib.auth.decorators import login_required
 from main.views import edit_news
 from main.views import delete_news
+from main.views import add_news_entry_ajax
 
 app_name = 'main'
 
@@ -20,4 +21,5 @@ urlpatterns = [
     path('login/', login_user, name='login'), 
     path('news/<uuid:id>/edit', edit_news, name='edit_news'),
     path('news/<uuid:id>/delete', delete_news, name='delete_news'),
+    path('create-news-ajax', add_news_entry_ajax, name='add_news_entry_ajax'),
 ]
